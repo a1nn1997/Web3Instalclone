422ad9168be03e35d2433d875a51de18f0f7e966
Aniruddh Singh <asb.aniruddh.singh@gmail.com>
2022-03-27 12:00:49 +0530

    smart contract1.0

--- src/contracts/Decentragram.sol
+++ src/contracts/Decentragram.sol
@@ -21,6 +21,15 @@ contract Decentragram {
     uint tipAmount,
     address payable author
   );
+
+  event ImageTipped(
+    uint id,
+    string hash,
+    string description,
+    uint tipAmount,
+    address payable author
+  );
+
   //Create Images
   function uploadImage(string memory _imgHash, string memory _description ) public{
     
@@ -33,7 +42,7 @@ contract Decentragram {
     // make sure uploader address exists
     require(msg.sender != address(0));
     //the above process os called validation
-    
+
     //Increment image ID
     imageCount ++;
     
@@ -44,5 +53,27 @@ contract Decentragram {
     emit ImageCreated(imageCount, _imgHash, _description,0, msg.sender);
   }
   //Tips Images
+  function tipImageOwner( uint _id) public payable{
+    
+    // Make sure the id is valid
+    require(_id > 0 && _id <= imageCount);
 
+    // Fetch the image
+    Image memory _image =images[_id];
+    
+    // Fetch the author
+    address payable _author = _image.author;
+
+    // Pay the author by send them cryptocurrency
+    address(_author).transfer(msg.value);
+
+    // Increment the tip amount
+    _image.tipAmount = _image.tipAmount + msg.value;
+
+    // Update the image
+    images[_id] = _image;
+
+    // trigger the event
+    emit ImageTipped(_id, _image.hash, _image.description, _image.tipAmount, _author);
+  }
 }
